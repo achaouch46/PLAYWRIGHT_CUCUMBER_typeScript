94d03d22e98b697db3ae3960fd7a9746f401f63a
achaouch46 <chaouchabdurrahmene@gmail.com>
2026-02-23 03:41:29 +0100

    message 7

--- src/test/features/api/loginApi.feature
+++ src/test/features/api/loginApi.feature
@@ -2,6 +2,7 @@ Feature: Users API
 
 @api @all
   Scenario: Get list of users
-    When I call the GET users API
-    Then the response status should be 200
-    And the response should contain users
+    When I call the GET users API 2
+    Then the response should contain user
+
+    
--- src/test/steps/api/users.steps.ts
+++ src/test/steps/api/users.steps.ts
@@ -1,19 +1,54 @@
-import { When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
-import { getApiContext } from '../../../helper/util/apiContext';
+// src/test/steps/api/users.steps.ts
+import { Before, Given, When, Then,After } from '@cucumber/cucumber';
+import { request, expect } from '@playwright/test';
 
-let response: any;
+// Définir un type pour le contexte partagé
+interface SharedContext {
+  apiContext?: any;
+  response?: any;
+  responseBody?: any;
+  userId?: number;
+}
 
-When('I call the GET users API', async function () {
-  const api = await getApiContext();
-  response = await api.get('/users?page=2');
+const sharedContext: SharedContext = {};
+
+Before(async function() {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  
+  try {
+    sharedContext.apiContext = await request.newContext({
+      baseURL: 'https://fakestoreapi.com',
+      ignoreHTTPSErrors: true,
+      timeout: 30000,
+    });
+  } catch (error) {
+    throw error;
+  }
 });
 
-Then('the response status should be {int}', async function (status: number) {
-  expect(response.status()).toBe(status);
+When("I call the GET users API {int}", async function(userId: number) {
+  try {
+    const response = await sharedContext.apiContext.get(`/users/${userId}`);
+    sharedContext.response = response;
+    sharedContext.userId = userId;
+    
+    if (response.ok()) {
+      sharedContext.responseBody = await response.json();
+    }
+  } catch (error) {
+    console.error('❌ Erreur requête:', error);
+    throw error;
+  }
 });
 
-Then('the response should contain users', async function () {
-  const body = await response.json();
-  expect(body.data.length).toBeGreaterThan(0);
+Then("the response should contain user", async function() {
+  expect(sharedContext.response.status()).toBe(200);
+  expect(sharedContext.responseBody.id).toBe(sharedContext.userId);
+  expect(sharedContext.responseBody.email).toBeDefined();
+});
+
+After(async function() {
+  if (sharedContext.apiContext) {
+    await sharedContext.apiContext.dispose();
+  }
 });
